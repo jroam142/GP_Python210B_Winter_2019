4cc1201d5a5253eae7fd758baba1d77bbcb68087
Jeremy <jereamon@gmail.com>
2019-03-16 11:58:48 -0700

    Removed my get_user_input function and put the input messages where input is called rather than in a separate dict. Meant to do this a while ago.

--- students/jeremy_m/lesson09_exercise/cli_main.py
+++ students/jeremy_m/lesson09_exercise/cli_main.py
@@ -8,6 +8,7 @@ import pathlib
 import time
 from donor_models import Donor, DonorCollection
 
+# This dict is just used to populate don_col.donors dict.
 donors = {'Charlize Theron': [134000],
           'Charlie Boorman': [15, 5],
           'James Franco': [25, 250, 2, 500],
@@ -16,29 +17,13 @@ donors = {'Charlize Theron': [134000],
           }
 
 user_input = ''
-
-messages = {'start': ("What would you like to do?\n"
-                      "1: to Send a Thank You\n"
-                      "2: Create a Report\n"
-                      "3 to create letters for all donors\n"
-                      "quit: to Quit"
-                      ),
-            'thanks': ("Please input the donors name "),
-            'thank_you_message': ("{:^42}\n"
-                                  "{:^42}\n"
-                                  "For your incredibly generous donation of:\n"
-                                  "{:>19}{:<23,}\n\n"),
-            'donor_letter': ("{:^41}\n"
-                             "Thank you so much for your generous donation of:\n"
-                             "{:>21}{:,}\n"
-                             "We will always remember your money fondly."),
-            'letters_confirmation': 'Is this ok?\nType y or n:'
-            }
+input_prompt = '\n\n---->'
 
 
 def letters_for_all():
-    """ This function creates a directory in the cwd and fills that with an
-        individual file (letter) for each donor.
+    """ 
+    This function creates a directory in the cwd and fills that with an
+    individual file (letter) for each donor.
     """
 
     clear_screen()
@@ -62,15 +47,20 @@ def get_donor_filename(donor_name):
 
 
 def get_letter_text(donor_name):
-    letter_text = messages['donor_letter'].format(
+    letter_text = ("{:^41}\n"
+                   "Thank you so much for your generous donation of:\n"
+                   "{:>21}{:,}\n"
+                   "We will always remember your money fondly.").format(
         donor_name, '$', int(don_col.donors[donor_name].donations[-1]))
     return letter_text
 
 
 def send_a_thank_you():
-    """ Gives a user the option to list all donors, or else will create a new
-        donor using the inputted name and adds their new donation using the
-        inputted amount """
+    """ 
+    Gives a user the option to list all donors, or else will create a new
+    donor using the inputted name and adds their new donation using the
+    inputted amount.
+    """
     clear_screen()
 
     donor_name_input, donation_amount = send_a_thank_you_inputs()
@@ -81,10 +71,11 @@ def send_a_thank_you():
 
 
 def send_a_thank_you_inputs():
-    """ Returns donors name and their recent donation amount after getting both
-        as input from the user.
+    """ 
+    Returns donors name and their recent donation amount after getting both
+    as input from the user.
     """
-    donor_name_input = get_user_input(messages['thanks'])
+    donor_name_input = input("Please input the donors name" + input_prompt)
 
     if donor_name_input == 'list':
         clear_screen()
@@ -92,13 +83,14 @@ def send_a_thank_you_inputs():
         for donor in donors:
             print(donor)
 
-        donor_name_input = get_user_input('\n\n' + messages['thanks'])
+        donor_name_input = input(
+            "\n\nPlease input the donors name" + input_prompt)
 
     # Keep asking for donation_amount until user enters a number.
     while True:
         try:
-            donation_amount = int(get_user_input(
-                'How much did {} contribute?'.format(donor_name_input)))
+            donation_amount = int(input(
+                'How much did {} contribute?{}'.format(donor_name_input, input_prompt)))
             break
         except ValueError:
             print('Please enter a number.')
@@ -108,14 +100,12 @@ def send_a_thank_you_inputs():
 
 def print_thank_you_message(donor_name_input):
     """ Print a formatted message with donors name and recent donation amount """
-    print(messages['thank_you_message'].format('Thank you so much',
-                                               donor_name_input, '$',
-                                               int(don_col.donors[donor_name_input].donations[-1])))
-
-
-def get_user_input(msg):
-    """ Gets a users input using msg as the prompt """
-    return input(msg + '\n\n---->')
+    print(("{:^42}\n"
+           "{:^42}\n"
+           "For your incredibly generous donation of:\n"
+           "{:>19}{:<23,}\n\n").format('Thank you so much',
+                                       donor_name_input, '$',
+                                       int(don_col.donors[donor_name_input].donations[-1])))
 
 
 def clear_screen():
@@ -131,15 +121,22 @@ main_menu_answers = {'1': send_a_thank_you,
 if __name__ == "__main__":
     clear_screen()
 
+    # This is just to populate the don_col.donors dict with some info to play with
     for donor, donations in donors.items():
         don_col.add_donor(donor, donations[0])
-        # if len(donations) > 1:
-        #     for donation in donations[1:]:
-        #         don_col.donors[donor].add_donation(donation)
+        if len(donations) > 1:
+            for donation in donations[1:]:
+                don_col.donors[donor].add_donation(donation)
+        print(don_col.donors[donor].donations)
 
     # Continue getting input until user types quit
     while user_input.lower() != 'quit':
-        user_input = get_user_input(messages['start'])
+        user_input = input(("What would you like to do?\n"
+                            "1: to Send a Thank You\n"
+                            "2: Create a Report\n"
+                            "3 to create letters for all donors\n"
+                            "quit: to Quit{}"
+                            ).format(input_prompt))
 
         try:
             main_menu_answers.get(user_input, 'nothing')()
